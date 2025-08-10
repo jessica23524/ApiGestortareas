fa0cdbaef67b753375ad57f802154484cd4123ea
jessica23524 <ja9385644@gmail.com>
2025-08-10 17:16:53 -0500

    Configuración de conexión con variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,34 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module'; // <--- importar aquí
+import { AuthModule } from './auth/auth.module';
 import { Task } from './tasks/task.entity';
 import { User } from './users/user.entity';
 
 @Module({
   imports: [
+    // Carga automáticamente el archivo .env y lo hace global
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Conexión a la base de datos
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'jessica',
-      database: 'tareas',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '5433', 10),
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'jessica',
+      database: process.env.DB_NAME || 'tareas',
       entities: [Task, User],
-      synchronize: true,
+      synchronize: true, // en producción puedes poner false
     }),
+
     TasksModule,
     UsersModule,
-    AuthModule, // <--- agregar aquí
+    AuthModule,
   ],
 })
 export class AppModule {}
-
